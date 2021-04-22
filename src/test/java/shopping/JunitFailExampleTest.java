eaa5a8168a20b7326a370ed93a5e6e2f1acfea9a
golf.atomic.tu@gmail <76625745+GolfWC@users.noreply.github.com>
2021-04-21 22:10:40 -0400

    test

--- src/test/java/shopping/JunitFailExampleTest.java
+++ src/test/java/shopping/JunitFailExampleTest.java
@@ -3,19 +3,17 @@ package shopping;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JunitFailExampleTest {
-
+	
 	@Test
 	public void towPlustwoEqual4() {
 		assertEquals("2+2 = 4",4,2+2);
 	}
 	
-	@Test
-	public void towPlustwoEqualFour() {
-		assertEquals("2+2 = 4",4,2+3);
-	}
+	
 	
 	boolean result = true;
 	@Test
@@ -23,9 +21,6 @@ public class JunitFailExampleTest {
 		assertTrue(result);
 	}
 	
-	@Test
-	public void testingAssertTrueWhitDescription() {
-		assertTrue("Result is true",!result);
-	}
+	
 	
 }
